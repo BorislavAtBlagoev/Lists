74b98ce1842930eee2b8cca26b48d34baf67d114
Борислав Благоев <46755995+BorislavAtBlagoev@users.noreply.github.com>
2019-04-08 14:04:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ListsExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string inputDigits = Console.ReadLine();
+            SumAdjacentEqualsDigits(inputDigits);
+            Console.ReadLine();
+
+        }
+
+        static void SumAdjacentEqualsDigits(string inputDigits)
+        {
+            var digits = inputDigits.Split(' ').Select(double.Parse).ToList();
+
+            for (int i = 1; i < digits.Count; i++)
+            {
+                if (digits[i] == digits[i - 1])
+                {
+                    digits[i] += digits[i - 1];
+                    digits.RemoveAt(i - 1);
+                    i = 0;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", digits));
+        }
+
+    }
+}
