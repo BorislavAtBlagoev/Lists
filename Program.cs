d61b5f9dd2baf56138579e5cf7fefa7526e9d8c3
Борислав Благоев <46755995+BorislavAtBlagoev@users.noreply.github.com>
2019-04-08 14:11:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ListExercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> digits = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            RemoveNegativesAndReverse(digits);
+            Console.ReadLine();
+        }
+
+        static void RemoveNegativesAndReverse(List<int> digits)
+        {
+            
+            for (int i = 0; i < digits.Count; i++)
+            {
+                if (digits[i] < 0)
+                {
+                    digits.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            if (digits.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+
+            digits.Reverse();
+
+            Console.WriteLine(string.Join(" ", digits));
+        }
+    }
+}
