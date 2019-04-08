8ee394cfbfa73bcb465cd7b0f6b3a39682fe8fe5
Борислав Благоев <46755995+BorislavAtBlagoev@users.noreply.github.com>
2019-04-08 14:10:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ListExercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfProducts = int.Parse(Console.ReadLine());
+            List<string> listOfProducts = new List<string>();
+
+            for (int i = 0; i < numberOfProducts; i++)
+            {
+                listOfProducts.Add(Console.ReadLine()); 
+            }
+
+            listOfProducts.Sort();
+
+            for (int i = 0; i < numberOfProducts; i++)
+            {
+                Console.WriteLine($"{i+1}.{listOfProducts[i]}");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
