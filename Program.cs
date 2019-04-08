f2d84614a04cc735927cb96b36e8d3e71f1f0a3b
Борислав Благоев <46755995+BorislavAtBlagoev@users.noreply.github.com>
2019-04-08 14:18:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ListExercise14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> reverseValues = Console.ReadLine()
+                                .Split('|')
+                                .Reverse()
+                                .ToList();
+            
+            List<int> number = new List<int>();
+            foreach (string p in reverseValues)
+            {
+                number.AddRange(p.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(int.Parse)
+                                    .ToList()
+                                     );
+            }
+
+            Console.WriteLine(string.Join(" ", number));
+            Console.ReadLine();
+        }
+    }
+}
