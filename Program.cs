0dfd22518bdb5bd9fc231c07df2301933a2cf687
Борислав Благоев <46755995+BorislavAtBlagoev@users.noreply.github.com>
2019-04-08 14:14:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ListExercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputWagons = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int maxCapacityOfWagons = int.Parse(Console.ReadLine());
+            AddMembersOrWagons(inputWagons, maxCapacityOfWagons);
+            Console.WriteLine(string.Join(" ", inputWagons));
+            Console.ReadLine();
+        }
+
+        static void AddMembersOrWagons(List<int> wagons, int maxCapacity)
+        {
+            string[] addMembers = Console.ReadLine().Split(" ").ToArray();
+
+            while (addMembers[0] != "end")
+            {
+                if (addMembers[0] == "Add")
+                {
+                    wagons.Add(int.Parse(addMembers[1]));
+                }
+                else
+                {
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if (maxCapacity >= wagons[i] + int.Parse(addMembers[0]))
+                        {
+                            wagons[i] += int.Parse(addMembers[0]);
+                            break;
+                        }
+                    }
+                }
+
+                addMembers = Console.ReadLine().Split(" ").ToArray();
+            }
+        }
+    }
+}
